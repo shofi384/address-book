f3722754f27c7f64b75938437633518f53b339e0
Shofiqur Rahman <shofi384@gmail.com>
2019-03-19 19:39:48 -0400

    Updates

---
+++ precache-manifest.c8502c0bf54601e6390ff8a0e10dcd91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d82b3f36d5ba20f8ffa988a8746d872f",
+    "url": "/shofi384/address-book/static/media/icon.d82b3f36.png"
+  },
+  {
+    "revision": "82d564b29f3d70a71c77",
+    "url": "/shofi384/address-book/static/js/runtime~main.deb137a6.js"
+  },
+  {
+    "revision": "36a3be251d8f7a025d76",
+    "url": "/shofi384/address-book/static/js/main.10a47279.chunk.js"
+  },
+  {
+    "revision": "d5315e1752b87b4959df",
+    "url": "/shofi384/address-book/static/js/2.26a0c5bd.chunk.js"
+  },
+  {
+    "revision": "36a3be251d8f7a025d76",
+    "url": "/shofi384/address-book/static/css/main.dd35c637.chunk.css"
+  },
+  {
+    "revision": "faaaf2efad452efba6835e7f89164aac",
+    "url": "/shofi384/address-book/index.html"
+  }
+];
