e233acc8ad26d69cf62ed1cc795618da0a1e6ffb
Shofiqur Rahman <shofi384@gmail.com>
2019-05-12 03:10:49 -0400

    separate data in a json file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './icon.png';
 import '../node_modules/font-awesome/css/font-awesome.min.css'; 
 import './App.css';
+import data from './data.json'
 
 class App extends Component {
   constructor(props) {
@@ -14,44 +15,7 @@ class App extends Component {
       Birthday: '',
       Telephone: '',
 
-      addressList: [
-      {
-        FirstName: "Cathy" ,
-        LastName: "Pierce",
-        Birthday: "9/14/1996",
-        Telephone: "200-910-8132"
-      },
-      {
-        FirstName: "Alfonso",
-        LastName: "Cooley",
-        Birthday: "8/10/1973",
-        Telephone: "200-657-9362"
-      },
-      {
-        FirstName: "Victor",
-        LastName: "Gordon",
-        Birthday: "8/3/1970",
-        Telephone: "200-661-9407"
-      },
-      {
-        FirstName: "Colleen",
-        LastName: "Wright",
-        Birthday: "10/28/1967",
-        Telephone: "200-250-7949"
-      },
-      {
-        FirstName: "James",
-        LastName: "Johnston",
-        Birthday: "5/11/1972",
-        Telephone: "200-645-3176"
-      },
-      {
-        FirstName: "Anna",
-        LastName: "Reyes",
-        Birthday: "9/10/1975",
-        Telephone: "200-707-8670"
-      }
-      ]
+      addressList: data
     };
 
     this.addAddress = this.addAddress.bind(this);
